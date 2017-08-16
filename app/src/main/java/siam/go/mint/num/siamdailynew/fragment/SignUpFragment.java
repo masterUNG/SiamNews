196da3209aad57a7b2788694b8149161e6a1cd77
MasterUNG <phrombutr@gmail.com>
2017-08-16 14:31:45 +0700

    Check Space OK

--- app/src/main/java/siam/go/mint/num/siamdailynew/fragment/SignUpFragment.java
+++ app/src/main/java/siam/go/mint/num/siamdailynew/fragment/SignUpFragment.java
@@ -6,15 +6,23 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 import siam.go.mint.num.siamdailynew.R;
+import siam.go.mint.num.siamdailynew.manage.MyAlert;
 
 /**
  * Created by Tong on 15/8/2560.
  */
 
-public class SignUpFragment extends Fragment{
+public class SignUpFragment extends Fragment {
+
+    //Explicit
+    private String nameString, surnameString, emailString,
+            userString, passwordString, genderString, divisionString, rePasswordString;
+
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater,
@@ -31,7 +39,54 @@ public class SignUpFragment extends Fragment{
         //Back Controller
         backController();
 
+        //Save Controller
+        saveController();
+
     }   // onActivityCreate
+
+    private void saveController() {
+        ImageView imageView = getView().findViewById(R.id.imvSave);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //Initial EditText
+                EditText nameEditText = getView().findViewById(R.id.edtName);
+                EditText surNameEditText = getView().findViewById(R.id.edtSurName);
+                EditText emailEditText = getView().findViewById(R.id.edtEmail);
+                EditText userEditText = getView().findViewById(R.id.edtUser);
+                EditText passwordEditText = getView().findViewById(R.id.edtPassword);
+                EditText RePasswordEditText = getView().findViewById(R.id.edtRePassword);
+
+                //Get Value to String
+                nameString = nameEditText.getText().toString().trim();
+                surnameString = surNameEditText.getText().toString().trim();
+                emailString = emailEditText.getText().toString().trim();
+                userString = userEditText.getText().toString().trim();
+                passwordString = passwordEditText.getText().toString().trim();
+                rePasswordString = RePasswordEditText.getText().toString().trim();
+
+                //Check Space
+                if (checkSpace()) {
+                    //Have Space
+                    MyAlert myAlert = new MyAlert(getActivity());
+                    myAlert.myDialog(getResources().getString(R.string.have_space),
+                            getResources().getString(R.string.message_have_space));
+                }
+
+
+            }   // onClick
+        });
+    }
+
+    private boolean checkSpace() {
+        return nameString.equals("") ||
+                surnameString.equals("") ||
+                emailString.equals("") ||
+                userString.equals("") ||
+                passwordString.equals("") ||
+                rePasswordString.equals("");
+    }
 
     private void backController() {
         ImageView imageView = getView().findViewById(R.id.imvBack);
