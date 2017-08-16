f9f8db9cdd0fd629229d81518f751957fb076fcb
MasterUNG <phrombutr@gmail.com>
2017-08-16 13:33:01 +0700

    Back Controller ok

--- app/src/main/java/siam/go/mint/num/siamdailynew/fragment/SignUpFragment.java
+++ app/src/main/java/siam/go/mint/num/siamdailynew/fragment/SignUpFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import siam.go.mint.num.siamdailynew.R;
 
@@ -16,8 +17,30 @@ import siam.go.mint.num.siamdailynew.R;
 public class SignUpFragment extends Fragment{
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_sign_up, container, false);
         return view;
+    }   // onCreateView
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        //Back Controller
+        backController();
+
+    }   // onActivityCreate
+
+    private void backController() {
+        ImageView imageView = getView().findViewById(R.id.imvBack);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getActivity().getSupportFragmentManager()
+                        .popBackStack();
+            }
+        });
     }
 }   //Main Class
